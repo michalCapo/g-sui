98b2f2b79979141f1926dc09c3712c6505c1cdb7
Michal Capo <michal.capo@gmail.com>
2025-12-28 11:58:54 +0100

    update: example

--- examples/pages/form.go
+++ examples/pages/form.go
@@ -1,16 +1,45 @@
 package pages
 
-import "github.com/michalCapo/g-sui/ui"
+import (
+	"fmt"
 
-func Submit(ctx *ui.Context) string {
-	return "ok"
+	"github.com/go-playground/validator/v10"
+	"github.com/michalCapo/g-sui/ui"
+)
+
+type formData struct {
+	Title string `validate:"required"`
+	Some  int    `validate:"required"`
 }
 
-func FormContent(ctx *ui.Context) string {
-	target := ui.Target()
-	some := ui.FormNew(ctx.Submit(Submit).Replace(target))
+func Submit(ctx *ui.Context) string {
+	form := formData{}
+	err := ctx.Body(&form)
+	if err != nil {
+		return render(ctx, &form, &err)
+	}
 
-	return ui.Div("max-w-5xl mx-auto flex flex-col gap-4")(
+	v := validator.New()
+	if err := v.Struct(form); err != nil {
+		return render(ctx, &form, &err)
+	}
+
+	ctx.Success("Form submitted successfully")
+
+	return render(ctx, &form, nil)
+}
+
+func render(ctx *ui.Context, data *formData, err *error) string {
+	target := ui.Target()
+	form := ui.FormNew(ctx.Submit(Submit).Replace(target))
+
+	result := "Form submit result will be displayed here."
+
+	if err == nil {
+		result = fmt.Sprintf("Form data: %+v", data)
+	}
+
+	return ui.Div("max-w-5xl mx-auto flex flex-col gap-4", target)(
 		ui.Div("text-2xl font-bold")("Form missmatch"),
 		ui.Div("text-gray-600")("Form input fields and submit button is defined outside html form element. This is useful when you want to reuse the form in multiple places."),
 
@@ -20,15 +49,20 @@ func FormContent(ctx *ui.Context) string {
 				ui.Div("text-gray-600 text-sm mb-4")("Form example with input fields and submit button."),
 			),
 
-			ui.Div("flex flex-col", target)(
-				"Form submit result will be displayed here.",
+			ui.ErrorForm(err, nil),
+
+			ui.Div("flex flex-col")(
+				result,
 			),
 
-			some.Render(),
-			some.Text("Title").Required().Render("Title"),
-			some.Number("Number").Render("Number"),
-			some.Hidden("Some", "number", "value", ui.Attr{Step: "123"}),
-			some.Button().Color(ui.Blue).Submit().Render("Submit"),
+			form.Render(),
+			form.Text("Title", data).Required().Render("Title"),
+			form.Hidden("Some", "int", 123),
+			form.Button().Color(ui.Blue).Submit().Render("Submit"),
 		),
 	)
+}
+
+func FormContent(ctx *ui.Context) string {
+	return render(ctx, &formData{}, nil)
 }
